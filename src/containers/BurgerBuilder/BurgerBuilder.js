82f5f7411a33984d15244221b698c8dc304a2d2c
James Farfan <jamesrojas710@gmail.com>
2020-11-27 13:40:52 -0500

    structuring burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Frag from '../../hoc/Frag';
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
     render() {
         return (
             <Frag>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Frag>
         );
