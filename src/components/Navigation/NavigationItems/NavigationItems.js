62a26c3db91507802352826a56c9d2215b71ac74
James Farfan <jamesrojas710@gmail.com>
2020-11-29 00:59:35 -0500

    added resuable Navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../NavigationItems/NavigationItems.css';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
+
+
+const navigationItems = () => (
+    <ul className='NavigationItems'>
+        <NavigationItem link='/' active>Burger Builder</NavigationItem>
+        <NavigationItem link='/'>Checkout</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
