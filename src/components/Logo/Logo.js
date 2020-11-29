937132045539285bde5b4e884eac2ed1fc768aa9
James Farfan <jamesrojas710@gmail.com>
2020-11-29 13:51:33 -0500

    responsive adjestments in toolbar and logo files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import '../Logo/Logo.css';
 
 const logo = (props) => (
-    <div className='Logo'>
+    <div className='Logo' style={{ height: props.height }}>
         <img src={burgerLogo} alt='Unique Burger' />
     </div>
 );
