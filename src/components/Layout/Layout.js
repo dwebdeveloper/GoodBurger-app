cfafa4747298d08ccd0fae90436628ec420842a0
James Farfan <jamesrojas710@gmail.com>
2020-11-29 13:10:51 -0500

    creating a responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Frag from '../../hoc/Frag';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Frag>
         <Toolbar />
+        <SideDrawer />
         <main className='Content'>
             {props.children}
         </main>
