a7aa031fd079b696e4c03f4de09c31f957eb44b6
James Farfan <jamesrojas710@gmail.com>
2020-11-29 23:57:15 -0500

    Reusing Backdrop for SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Frag from '../../hoc/Frag';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Frag>
-        <Toolbar />
-        <SideDrawer />
-        <main className='Content'>
-            {props.children}
-        </main>
-    </Frag>
+class Layout extends Component {
+    state = {
+        showSideDrawer: true,
+    };
 
-);
+    sideDrawerClosed = () => {
+        this.setState({ showSideDrawer: false });
+    };
 
-export default layout;
+
+    render() {
+        return (
+            <Frag>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerClosed} />
+                <main className='Content'>
+                    {this.props.children}
+                </main>
+            </Frag>
+
+        );
+    }
+
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,31 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import '../SideDrawer/SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Frag from '../../../hoc/Frag';
 
 const sideDrawer = (props) => {
+    let attachedClasses = ['SideDrawer', 'Close'];
+    if (props.open) {
+        attachedClasses = ['SideDrawer', 'Open'];
+    }
+
+
+
+
     return (
-        <div className='SideDrawer'>
-            <Logo height='11%' marginBottom='32px' />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Frag>
+            <Backdrop show={props.open} clicked={props.close} />
+
+            <div className={attachedClasses.join(' ')}>
+
+                <Logo height='11%' marginBottom='32px' />
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Frag >
+
     );
 };
 
