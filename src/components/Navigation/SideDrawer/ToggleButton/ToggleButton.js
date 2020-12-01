09164764e4bcd039246b764845e2c095a6f23982
James Farfan <jamesrojas710@gmail.com>
2020-11-30 20:45:54 -0500

    added hamburger icon

--- src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
+++ src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
@@ -2,7 +2,11 @@ import React from 'react';
 import '../ToggleButton/ToggleButton.css'
 
 const toggleButton = (props) => (
-    <div className='ToggleButton' onClick={props.clicked}>Menu</div>
+    <div className='ToggleButton' onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default toggleButton;
